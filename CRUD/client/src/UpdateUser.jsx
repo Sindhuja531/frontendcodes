f611f44cdd8de11b2eca261e1fa81cee267fd784
Sindhuja531 <sindhujapulumati31@yahoo.com>
2024-01-24 00:38:35 -0700

    crud

--- CRUD/client/src/UpdateUser.jsx
+++ CRUD/client/src/UpdateUser.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function UpdateUser() {
